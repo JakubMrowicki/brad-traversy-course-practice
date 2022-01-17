231aea7dd1dde70102153125fa49bdc02f40f080
Jakub Mrowicki <jakubmrowicki96@gmail.com>
2022-01-17 17:49:57 +0000

    end of section 4

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: '13c6ab7c-6950-48b3-9ee6-3a3e153a7b1a',
+    rating: 10,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+  {
+    id: 'ae5b2488-8401-4c4c-972b-d22aec1a6eab',
+    rating: 9,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+  {
+    id: '7ca2b7ad-7a1b-4b3c-879f-a4f22ac72bcd',
+    rating: 8,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+]
+
+export default FeedbackData
