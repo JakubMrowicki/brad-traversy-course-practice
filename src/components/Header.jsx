9649fb30b1766dec59b6f22aa5fe2fc7c75cccaa
Jakub Mrowicki <jakubmrowicki96@gmail.com>
2022-01-17 18:28:51 +0000

    end of section 5

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 function Header({ title }) {
   return (
     <header>
       <div className='container'>
-        <h2>{title}</h2>
+        <Link to='/'>
+          <h2>{title}</h2>
+        </Link>
       </div>
     </header>
   )
